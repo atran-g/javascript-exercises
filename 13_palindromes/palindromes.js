1f9fdf5d62c7d55db377c55d0590fac659b1b6cd
atran-g <atran_14@outlook.com>
2025-07-02 11:47:55 +0300

    palindromes passed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (w) {
+	const alphaNum = "abcdefghijklmnopqrstuvwxyz1234567890";
+	const wordArr = w.toLowerCase().split("");
+	const cleanWordArr = wordArr.filter(letter => alphaNum.split("").includes(letter));
 
+	let reversedCleanWordArr = [];
+	cleanWordArr.map(item => reversedCleanWordArr.unshift(item));
+
+	console.log(w);
+
+	console.log(cleanWordArr, reversedCleanWordArr);
+
+	return cleanWordArr.join() === reversedCleanWordArr.join();
 };
 
 // Do not edit below this line
