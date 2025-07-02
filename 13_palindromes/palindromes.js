75eaa9383ede9ae3ec387867f7c96fa984e409f2
atran-g <atran_14@outlook.com>
2025-07-02 20:49:38 +0300

    clean up on palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,14 +1,11 @@
 const palindromes = function (w) {
 	const alphaNum = "abcdefghijklmnopqrstuvwxyz1234567890";
+
 	const wordArr = w.toLowerCase().split("");
 	const cleanWordArr = wordArr.filter(letter => alphaNum.split("").includes(letter));
 
 	let reversedCleanWordArr = [];
 	cleanWordArr.map(item => reversedCleanWordArr.unshift(item));
-
-	console.log(w);
-
-	console.log(cleanWordArr, reversedCleanWordArr);
 
 	return cleanWordArr.join() === reversedCleanWordArr.join();
 };
