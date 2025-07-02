81359ae3ea91626f9890ea2084a5e1f7acc5b1ba
atran-g <atran_14@outlook.com>
2025-07-02 08:22:05 +0300

    solved all

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,41 @@
-const add = function() {
-	
+const add = function (l, r) {
+	return l + r;
 };
 
-const subtract = function() {
-	
+const subtract = function (l, r) {
+	return l - r;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((total, currentItem) => {
+		return total + currentItem;
+	}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((total, currentItem) => {
+		return total * currentItem;
+	}, 1);
 };
 
-const power = function() {
-	
+const power = function (l, r) {
+	return l ** r;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	let fact = 1;
+	for (let i = num; i > 0; i--) {
+		fact *= i;
+	}
+	return fact;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
