3123dbb14be87f2fb4a5018a77516c1a40d31fc7
atran-g <atran_14@outlook.com>
2025-07-02 21:05:08 +0300

    solved fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+	const intN = +n;
+	let arr = [1, 1];
 
+	if (intN === 0) return 0;
+	if (intN < 0) return "OOPS";
+
+	for (let i = 0; i < intN - 2; i++) {
+		arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+	}
+
+	return arr[arr.length - 1];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+/**
+ * [1, 1, 2, 3, 5]
+ * start with 1
+ * next number is always 1
+ * add 1 and 1
+ * push 2 to array
+ * add arr[arr.length-1] and arr[arr.length -2]
+ * push result
+ * do this n number of times
+ */
