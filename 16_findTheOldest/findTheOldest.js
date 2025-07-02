f16abd149339e04c8a8c2f4404ef08eb287bc0e6
atran-g <atran_14@outlook.com>
2025-07-02 21:28:54 +0300

    solved findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (peopleArr) {
+	let oldestIndex = 0;
+	peopleArr.map((person, index) => {
+		const today = new Date().getFullYear();
+		if (
+			(person.yearOfDeath || today) - person.yearOfBirth >
+			(peopleArr[oldestIndex].yearOfDeath || today) - peopleArr[oldestIndex].yearOfBirth
+		) {
+			oldestIndex = index;
+		}
+	});
+	return peopleArr[oldestIndex];
 };
 
 // Do not edit below this line
