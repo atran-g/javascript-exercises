4b812d3dfbc6ed2c566721fbbfe1b02d56e76831
atran-g <atran_14@outlook.com>
2025-10-18 17:06:32 +0300

    repeatString done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+	if (num < 0) return "ERROR";
+	let repeatArr = [];
+	for (let i = 0; i < num; i++) {
+		repeatArr.push(str);
+	}
+	return repeatArr.join("");
 };
 
 // Do not edit below this line
