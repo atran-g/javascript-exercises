10dd12a5c964d5f1bcb10eb496beaa7e7d781d5e
atran-g <atran_14@outlook.com>
2025-10-18 20:07:06 +0300

    08 to 11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+//Implement a function that takes 2 positive integers and
+//returns the sum of every integer between (and including) them:
+const sumAll = function (first, second) {
+	if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second)) {
+		console.log(first, second);
+		return "ERROR";
+	}
 
+	let start = first;
+	let end = second;
+
+	if (first > second) {
+		start = second;
+		end = first;
+	}
+	let sum = 0;
+	for (let i = start; i <= end; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (deg) {
+	const res = (deg - 32) * (5 / 9);
+	return Math.round(res * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (deg) {
+	const res = deg * (9 / 5) + 32;
+	return Math.round(res * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
